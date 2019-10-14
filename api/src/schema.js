3c3e86d6887394c1a17662e88111bc2e1c766fcb
Scott Moss <willscottmoss@gmail.com>
2019-10-13 22:23:27 -0500

    chore

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,10 +4,7 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  type User {
-    id: ID!
-    username: String!
-  }
+  
 
 `;
 
