3acbc6033df5e36296e380ced0dfc8ad6488ab50
cecil john tantay <cjbtantay@gmail.com>
2019-12-31 19:15:20 -0800

    complete typedef

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,7 +4,22 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID
+    username: String
+  }
+
+  type Pet {
+    id: ID
+    createdAt: Int
+    name: String
+    type: String
+  }
+
+  type Query {
+    user: User
+    pet: Pet
+  }
 
 `;
 
--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const {models, db} = require('./db')
 
-const server = new ApolloServer()
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
