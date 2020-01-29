137e7c41509dfdfad706b31aa69e9be277b417a8
HarryFray <nick.fray@merlinlabs.com>
2020-01-29 16:22:42 -0600

    feat(site): read and write to apollo db
    - hook up apollo client
    - create query for pets
    - create mutation to adding pets

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,10 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 
-/**
- * Create a new apollo client and export as default
- */
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000/" }),
+  cache: new InMemoryCache()
+});
+
+export default client;
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider } from '@apollo/react-hooks'
-import App from './components/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloProvider } from "@apollo/react-hooks";
+import App from "./components/App";
+import "./index.css";
+import client from "./client";
 
 const Root = () => (
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>
-)
+);
 
-ReactDOM.render(<Root />, document.getElementById('app'))
+ReactDOM.render(<Root />, document.getElementById("app"));
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
