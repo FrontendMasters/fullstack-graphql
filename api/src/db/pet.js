8abfd8c320caeda103cec2ac38064d6bf1d14461
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-09-08 23:03:57 +0530

    server fixes

--- api/src/db/pet.js
+++ api/src/db/pet.js
@@ -1,30 +1,27 @@
-const nanoid = require('nanoid')
+const nanoid = require("nanoid");
 
-const createPetModel = db => {
+const createPetModel = (db) => {
   return {
     findMany(filter) {
-      return db.get('pet')
+      return db
+        .get("pet")
         .filter(filter)
-        .orderBy(['createdAt'], ['desc'])
-        .value()
+        .orderBy(["createdAt"], ["desc"])
+        .value();
     },
 
     findOne(filter) {
-      return db.get('pet')
-        .find(filter)
-        .value()
+      return db.get("pet").find(filter).value();
     },
 
     create(pet) {
-      const newPet = {id: nanoid(), createdAt: Date.now(), ...pet}
-      
-      db.get('pet')
-        .push(newPet)
-        .write()
+      const newPet = { id: nanoid(), createdAt: Date.now(), ...pet };
 
-      return newPet
-    }
-  }
-}
+      db.get("pet").push(newPet).write();
 
-module.exports = createPetModel
+      return newPet;
+    },
+  };
+};
+
+module.exports = createPetModel;
--- api/src/db/user.js
+++ api/src/db/user.js
@@ -1,20 +1,18 @@
-const nanoid = require('nanoid')
+const nanoid = require("nanoid");
 
-const createUserModel = db => {
+const createUserModel = (db) => {
   return {
     findOne() {
-      return db.get('user')
-        .value()
+      return db.get("user").value();
     },
 
     create(user) {
-      const newUser = {id: nanoid(), createdAt: Date.now(), ...user}
-      db.set('user', newUser)
-        .write()
-  
-      return newUser
-    }
-  }
-}
+      const newUser = { id: nanoid(), createdAt: Date.now(), ...user };
+      db.set("user", newUser).write();
 
-module.exports = createUserModel
+      return newUser;
+    },
+  };
+};
+
+module.exports = createUserModel;
