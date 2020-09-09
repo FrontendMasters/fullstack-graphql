abe717ccae5059e4cc5a05f5cabe52ad2f222679
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-09-09 17:29:23 +0530

    update cache done

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,26 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
+import { ApolloLink } from "apollo-link";
 import gql from "graphql-tag";
+import { setContext } from "apollo-link-context";
 
 /**
  * Create a new apollo client and export as default
  */
+const http = new HttpLink({ uri: "http://localhost:4000/" });
+const delay = setContext(
+  (request) =>
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success();
+      }, 800);
+    })
+);
+const link = ApolloLink.from([delay, http]);
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({ link, cache });
+
+export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,12 +5,53 @@ import PetsList from "../components/PetsList";
 import NewPetModal from "../components/NewPetModal";
 import Loader from "../components/Loader";
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
+const NEW_PET = gql`
+  mutation CreateANewPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
 export default function Pets() {
   const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet] = useMutation(NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const { pets } = cache.readQuery({ query: ALL_PETS });
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet, ...pets] },
+      });
+    },
+  });
 
   const onSubmit = (input) => {
     setModal(false);
+    createPet({ variables: { newPet: input } });
   };
+
+  if (loading || newPet.loading) {
+    return <Loader />;
+  }
+
+  if (error || newPet.error) {
+    return <p>error</p>;
+  }
 
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
@@ -30,7 +71,7 @@ export default function Pets() {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   );
