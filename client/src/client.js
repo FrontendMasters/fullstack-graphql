ec15adad3da244cf342f202d48b3f263f2e1b40e
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-05 22:55:45 -0400

    link and cache defined

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,13 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
 /**
  * Create a new apollo client and export as default
  */
+
+// link is a network connection beteen Apollo server and client
+const link = new HttpLink({ uri: "http://localhost:4000/" });
+// apollo gives us a cache and we call it here
+const cache = new InMemoryCache()
