1dcbcf10f8df8bad2abe502e570b6a9e815a5aef
Rafe <rafenasir@gmail.com>
2021-07-06 16:11:31 +0200

    first client query with sample api

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,29 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
 /**
  * Create a new apollo client and export as default
  */
+
+const link = new HttpLink({ uri: "https://rickandmortyapi.com/graphql" });
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+const query = gql`
+  {
+    characters {
+      results {
+        name
+      }
+    }
+  }
+`;
+client.query({ query }).then((result) => console.log(result));
+
+export default client;
