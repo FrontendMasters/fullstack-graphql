de785756deb992e32606849442bdcb8c0bd0f3aa
Mikolaj Marcoll <mikolaj.marcoll@htdevelopers.com>
2021-07-30 12:51:07 +0200

    implement first graphQL query

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -1,16 +1,16 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync('api/src/db/db.json')
-const db = low(adapter)
+const adapter = new FileSync("api/src/db/db.json");
+const db = low(adapter);
 
-const createPetModel = require('./pet')
-const createUserModel = require('./user')
+const createPetModel = require("./pet");
+const createUserModel = require("./user");
 
 module.exports = {
   models: {
     Pet: createPetModel(db),
     User: createUserModel(db),
   },
-  db
-}
+  db,
+};
