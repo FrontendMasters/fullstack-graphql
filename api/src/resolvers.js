f569d7d749d453e375fb2a69b3d77e68140c08e2
Mohit pal <mohitpal9513@gmail.com>
2023-04-21 11:59:40 +0530

    do graphql

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,12 +5,30 @@
 
 module.exports = {
   Query: {
-    
+    pets(_,{input},ctx){
+      return ctx.models.Pet.findMany(input)
+    },
+    pet(_,{input},ctx){
+      console.log('Query =>pet')
+      return ctx.models.Pet.findOne(input)
+    }
   },
   Mutation: {
-    
+    newPet(_,{input},ctx){
+      const pet = ctx.models.Pet.create(input)
+      return pet
+    },
+    updatePet(_,{input},ctx){
+      const pet = ctx.models.Pet.update(input.id,input.updates)
+      return pet
+
+    }
   },
   Pet: {
+    owner(_,__,ctx){
+      console.log('PET => owner')
+      return ctx.models.User.findOne()
+    },
     img(pet) {
       return pet.type === 'DOG'
         ? 'https://placedog.net/300/300'
@@ -18,6 +36,9 @@ module.exports = {
     }
   },
   User: {
-    
+    pets(_,__,ctx){
+      console.log('User =>pets')
+      return ctx.models.Pet.findMany()
+    }
   }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,7 +4,37 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id:ID!
+    username: String!
+    pets:[Pet]!
+  }
+  type Pet{
+    id:ID!
+    createdAt: String!
+    name:String!
+    type:String!
+    img:String
+    owners:User!
+  }
+
+  input PetInput {
+     name: String
+     type: String
+  }
+  input NewPetInput{
+    name: String!
+    type: String!
+  }
+
+  type Query{
+    pets(input : PetInput):[Pet]!
+    pet(input: PetInput): Pet
+  }
+
+  type Mutation{
+    newPet(input:NewPetInput!):Pet!
+  }
 
 `;
 
